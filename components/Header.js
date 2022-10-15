c402371b11d43ec19fb9ed49a32acfd38f1fef2d
Varad Gundap <varad615@gmail.com>
2022-10-15 23:52:23 +0530

    fix update

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,20 @@
 import Image from "next/image";
-import MyDropdown from "./Dropdown/MyDropdown";
 
 const Header = () => {
-    const styles = {
-        wrapper: 'flex p-5 bg-white',
-        content: 'w-full flex-1 flex justify-end ',
-        logoContainer: 'flex items-center flex-start',
-        logo: 'object-contain cursor-pointer',
-        profileBut: 'bg-black text-white py-2 px-4 rounded-full cursor-pointer',
-    
-      };
+  const styles = {
+    wrapper: "flex p-5 bg-white",
+    content: "w-full flex-1 flex justify-end ",
+    logoContainer: "flex items-center flex-start",
+    logo: "object-contain cursor-pointer",
+    profileBut: "bg-black text-white py-2 px-4 rounded-full cursor-pointer"
+  };
   return (
     <div className={styles.wrapper}>
-    <div className={styles.content}>
+      <div className={styles.content}>
         {/* <div className={styles.profileBut}>T</div> */}
-        <MyDropdown />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
